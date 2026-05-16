1c034939c2df8ef213e29b9835eda4676be956c2
王俊麟 <wangjl@wangjunlindeMacBook-Air.local>
2026-05-15 21:46:02 -0400

    Rename diffusion_policy_3d to fgo

--- fgo/gym_util/mjpc_wrapper.py
+++ fgo/gym_util/mjpc_wrapper.py
@@ -3,7 +3,7 @@ import numpy as np
 import pytorch3d.ops as torch3d_ops
 import torch
 from termcolor import cprint
-from diffusion_policy_3d.gym_util.mujoco_point_cloud import PointCloudGenerator
+from fgo.gym_util.mujoco_point_cloud import PointCloudGenerator
 from typing import NamedTuple, Any
 from dm_env import StepType
 
